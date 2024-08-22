d468a5f3aa82d5df0174ebf66c0786d934eb3488
Luis Fernando Bautista Alcozer <158125083+FerFerFer35@users.noreply.github.com>
2024-08-22 16:11:03 -0600

    Add files via upload

---
+++ CRW.py
@@ -0,0 +1,30 @@
+#The first line
+import pandas as pd
+
+#Creating data
+#Object in panas: DataFrame and Series
+
+#Creating a DataFrame
+p = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
+print(p)
+
+print('----------------------------------')
+
+#DataFrame not limited for a int values
+d = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+print(d)
+
+print('----------------------------------')
+
+#Index: Use to name the rows (Out of the dictionary)
+f = pd.DataFrame({'Luis': ['Me gusta', 'No me gusta'],
+                'Fernando': ['Me encanta', 'No me encanta']},
+                index=['Opinion 1', 'Opinion 2'])
+print(f)
+
+print('----------------------------------')
+
+
+#Creating a Series
+s = pd.Series([1, 2, 3, 4, 5])
+print(s)
